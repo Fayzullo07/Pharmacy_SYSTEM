6529403aeb77e11feceec23ecebc0bc3da45964f
Fayzullo07 <jorayevfayzillo77@gmail.com>
2023-07-03 21:37:21 +0500

    search

--- src/utils/SearchInput.jsx
+++ src/utils/SearchInput.jsx
@@ -1,36 +1,27 @@
 import React, { useState } from "react";
 
 import "./SearchInput.css";
-import { toast } from "react-toastify";
 
 const SearchInput = ({ search, setSearch, setPage }) => {
-  const [value, setValue] = useState("");
-
-  const handleSubmit = () => {
-    if (value && value.length < 3) {
-      toast.warning("Eng kami 3ta symbol bo'lishi kerak!");
-      return;
-    }
-    setSearch(value);
-    setPage(1);
-  };
   return (
     <div className="search">
       <label>
+        <i
+          class="fa-solid fa-magnifying-glass text-muted"
+        />
         <input
           type="text"
           placeholder={`Izlash . . . `}
-          value={value}
-          onChange={e => setValue(e.target.value)}
+          value={search}
+          onChange={e => {
+            setSearch(e.target.value);
+            setPage(1);
+          }}
           onKeyDown={e => {
             if (e.key == "Enter") {
               handleSubmit();
             }
           }}
-        />
-        <i
-          class="fa-solid fa-magnifying-glass cursor_pointer"
-          onClick={handleSubmit}
         />
       </label>
     </div>
