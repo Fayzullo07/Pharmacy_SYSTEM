a2ab20052bec6ce2ee14ddfc38880f6b08f02b09
Fayzullo07 <jorayevfayzillo77@gmail.com>
2023-07-12 09:49:59 +0500

    date and pharmacy name

--- src/layout/UserEmployee/EditReport/EditReportWorker.jsx
+++ src/layout/UserEmployee/EditReport/EditReportWorker.jsx
@@ -29,6 +29,7 @@ const EditReportWorker = ({ user }) => {
   const { deteils } = reduxData.deteils;
 
   const [shows, setShows] = useState("1");
+    const savedUser = localStorage.getItem("user");
 
   const { data: remeinder, isLoading } = useQuery({
     queryKey: ["remeinder", shows],
@@ -49,8 +50,7 @@ const EditReportWorker = ({ user }) => {
 
   const { t: g } = useTranslation("translation", { keyPrefix: "Global" });
 
-  return (
-    <div className="d-flex">
+  return <div className="d-flex">
       <Navbar />
       <div className={`container_g ${toggle ? "" : "active"}`}>
         <Topbar>
@@ -69,56 +69,51 @@ const EditReportWorker = ({ user }) => {
               {shows == "11" && g(47)}
               {shows == "12" && g(60)}
             </h2>
-            {shows == "1" &&
-              <h2 id="remeinder">
+            {shows == "1" && <h2 id="remeinder">
                 <span>{g(61)}: </span>
                 <b>
-                  {isLoading
-                    ? <span>
+                  {isLoading ? <span>
                         {number_0}
-                      </span>
-                    : remeinder.data.price == 0
-                      ? <span>
+                      </span> : remeinder.data.price == 0 ? <span>
                           {number_0}
-                        </span>
-                      : formatNumber(remeinder.data.price)}
-                </b>{" "}
-                <span> UZS</span>
+                        </span> : formatNumber(remeinder.data.price)}
+                </b> <span> UZS</span>
               </h2>}
           </div>
         </Topbar>
+        <div className="header_flex mb-2">
+          {<div className="border border-primary p-1 mx-2 rounded">
+              <span>
+                {today}
+              </span>
+              <i className="fa fa-clock mx-1 text-warning" />
+            </div>}
+          <div className="border border-primary p-1 mx-2 rounded">
+            <span>
+              <span>
+                {JSON.parse(savedUser)?.user.pharmacy_name.length > 16 ? `${JSON.parse(savedUser)?.user.pharmacy_name.substring(0, 16)}. . .` : JSON.parse(savedUser)?.user.pharmacy_name}
+              </span>
+            </span>
+          </div>
+        </div>
         <div className="header_flex mb-2">
           <SideBarEdit setShows={setShows} shows={shows} />
         </div>
 
         {shows == "1" && <TodayInComes deteils={deteils} getData={getData} />}
         {shows == "2" && <TodayExpenses deteils={deteils} getData={getData} />}
-        {shows == "3" &&
-          <TodayExPenseToFirm deteils={deteils} getData={getData} />}
-        {shows == "4" &&
-          <TodayTradeToDebt is_client={true} getData={getData} />}
-        {shows == "5" &&
-          <TodayTradeToDebtRepay
-            deteils={deteils}
-            is_client={true}
-            getData={getData}
-          />}
+        {shows == "3" && <TodayExPenseToFirm deteils={deteils} getData={getData} />}
+        {shows == "4" && <TodayTradeToDebt is_client={true} getData={getData} />}
+        {shows == "5" && <TodayTradeToDebtRepay deteils={deteils} is_client={true} getData={getData} />}
         {shows == "6" && <TodayToDebt is_client={false} getData={getData} />}
-        {shows == "7" &&
-          <TodayToDeptRepay
-            deteils={deteils}
-            is_client={false}
-            getData={getData}
-          />}
+        {shows == "7" && <TodayToDeptRepay deteils={deteils} is_client={false} getData={getData} />}
         {shows == "8" && <TodayDebt deteils={deteils} getData={getData} />}
         {shows == "9" && <TodayDebtRepay deteils={deteils} getData={getData} />}
         {shows == "10" && <TodayReturn deteils={deteils} getData={getData} />}
         {shows == "11" && <TodayDiscount getData={getData} />}
-        {shows == "12" &&
-          <TodayExpenseToAccounts deteils={deteils} getData={getData} />}
+        {shows == "12" && <TodayExpenseToAccounts deteils={deteils} getData={getData} />}
       </div>
-    </div>
-  );
+    </div>;
 };
 
 export default EditReportWorker;
